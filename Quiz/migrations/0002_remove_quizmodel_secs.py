b00d2c04d37b54d4a41d5113a29597adf32a1bba
Pranav696 <76125614+Pranav696@users.noreply.github.com>
2022-12-06 20:51:58 +0530

    Add files via upload

---
+++ Quiz/migrations/0002_remove_quizmodel_secs.py
@@ -0,0 +1,16 @@
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Quiz', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='quizmodel',
+            name='secs',
+        ),
+    ]
